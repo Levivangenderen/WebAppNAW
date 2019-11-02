7d34eaa8412f30a14ec54b40fcb301208250eaf8
Levi Genderen van <Levi_van_Genderen@hotmail.com>
2019-11-02 19:58:07 +0100

    Commit and Push, Project to GitHub!

---
+++ WebAppNAW/NAWplusDB.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebAppNAW
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(IsPostBack)
+            {
+                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["LeviConnectionString"].ConnectionString);
+                conn.Open();
+
+                string checkuser = "select count (*) from [Table] where UserName = '" + TextBoxUN.Text + "'";
+                SqlCommand com = new SqlCommand(checkuser, conn);
+                int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+                if(temp == 1)
+                {
+                    Response.Write("User already Exists");
+                }
+
+                
+                conn.Close();
+            } 
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["LeviConnectionString"].ConnectionString);
+                conn.Open();
+
+                //string insertQuery = "insert into [Table] (UserName,Email,Password,Country) values (@UnameLVG ,@emailLVG ,@passwordLVG ,@countryLVG)";
+                string insertQuery = "insert into [Table] (UserName,Email,Password,Country) values (@UnameLVG ,@emailLVG ,@passwordLVG ,@countryLVG)";
+                SqlCommand com = new SqlCommand(insertQuery, conn);
+                com.Parameters.AddWithValue("@UnameLVG", TextBoxUN.Text);
+                com.Parameters.AddWithValue("@emailLVG", TextBoxEmail.Text);
+                com.Parameters.AddWithValue("@passwordLVG", TextBoxPass.Text);
+                com.Parameters.AddWithValue("@countryLVG", DropDownListCtry.SelectedItem.ToString());
+
+                com.ExecuteNonQuery();
+                Response.Redirect("leviManager.aspx");
+                Response.Write("Registration is successful, LVG");
+
+                conn.Close();
+
+                //Response.Write("Your Registration is Succesfull");
+            }
+                catch(Exception ex)
+            {
+                Response.Write("ErrorLVG:" +ex.ToString());
+            }
+        }
+    }
+}
---
+++ WebAppNAW/leviManager.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="leviManager.aspx.cs" Inherits="WebAppNAW.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 477px;
+            height: 313px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server" class="auto-style1">
+        <div>
+            <asp:SqlDataSource ID="SqlDataSource_leviRegistration" runat="server" ConnectionString="<%$ ConnectionStrings:LeviConnectionString %>" SelectCommand="SELECT * FROM [Table]"></asp:SqlDataSource>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="Id" DataSourceID="SqlDataSource_leviRegistration" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:CommandField ShowSelectButton="True" />
+                <asp:BoundField DataField="Id" HeaderText="Id" ReadOnly="True" SortExpression="Id" />
+                <asp:BoundField DataField="UserName" HeaderText="UserName" SortExpression="UserName" />
+                <asp:BoundField DataField="Email" HeaderText="Email" SortExpression="Email" />
+                <asp:BoundField DataField="Password" HeaderText="Password" SortExpression="Password" />
+                <asp:BoundField DataField="Country" HeaderText="Country" SortExpression="Country" />
+            </Columns>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
